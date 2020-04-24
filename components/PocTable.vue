37e1c2274110fe466cc3dadc66e94d549bc6b3fd
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-04-24 15:06:34 -0400

    in the middle of updating front end to connect to api

---
+++ components/PocTable.vue
@@ -0,0 +1,18 @@
+<script>
+import PocService from '../requests/PocService';
+export default {
+  name: 'PocServiceComponent',
+  data(){
+    return {
+      pocs: [],
+      error: '',
+      text: ''
+    }
+  },
+  async created() {
+    try{
+      this.pocs = await PocService.getAllPocs();
+    } catch(err){this.error = err.message;}
+  }
+}
+</script>
