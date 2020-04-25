9eaa2ab005a2cf3dad043d4e2289ab6afb9f25db
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-04-24 20:13:11 -0400

    fixing

--- pages/index.vue
+++ pages/index.vue
@@ -35,11 +35,7 @@ export default {
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
