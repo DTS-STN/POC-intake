e20596be4c227c77e8c25a4e41e8382e60012629
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-04-24 22:46:40 -0400

    adding environment variables and doing tweaks

--- requests/PocService.js
+++ requests/PocService.js
@@ -1,27 +1,8 @@
 import axios from 'axios';
-
+const api=`${process.env.VUE_APP_API_URL}`;
 class PocService{
-////To make calls for all PoCs
-
-//   static getAllPocs() {
-//     return new Promise(async (resolve, reject) => {
-//       try {
-//         const res = await axios.get(url + 'pocs');
-//         const data = res.data;
-//         resolve(
-//           data.map(poc => ({
-//             ...poc,
-//             createdAt: new Date(poc.createdAt)
-//           }))
-//         );
-//       } catch (err){
-//         reject(err);
-//       }
-//     })
-//   }
-
   static insertPoc(fname, lname, email, message){
-    return axios.post('http://localhost:3000/api/insertpoc', null, { params: {fname: fname, lname: lname, email: email, message: message}});
+    return axios.post(`${api}insertpoc`, null, { params: {fname: fname, lname: lname, email: email, message: message}});
   }
 }
 export default PocService;
