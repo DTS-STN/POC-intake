e4fadb06c0f856c45c22d34c75a842f2be510d23
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-04-24 20:08:56 -0400

    API is running.  working on fixing tests

--- requests/PocService.js
+++ requests/PocService.js
@@ -1,27 +1,27 @@
-import axios from axios;
-
-const url = 'http://localhost:3000/api/';
+import axios from 'axios';
 
 class PocService{
-  static getAllPocs() {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const res = await axios.get(url + 'pocs');
-        const data = res.data;
-        resolve(
-          data.map(post => ({
-            ...post,
-            createdAt: new Date(post.createdAt)
-          }))
-        );
-      } catch (err){
-        reject(err);
-      }
-    })
-  }
+////To make calls for all PoCs
 
-  static insertPoc(text){
-    return axios.post(url + 'insertpoc', { text });
+//   static getAllPocs() {
+//     return new Promise(async (resolve, reject) => {
+//       try {
+//         const res = await axios.get(url + 'pocs');
+//         const data = res.data;
+//         resolve(
+//           data.map(poc => ({
+//             ...poc,
+//             createdAt: new Date(poc.createdAt)
+//           }))
+//         );
+//       } catch (err){
+//         reject(err);
+//       }
+//     })
+//   }
+
+  static insertPoc(fname, lname, email, message){
+    return axios.post('http://localhost:3000/api/insertpoc', null, { params: {fname: fname, lname: lname, email: email, message: message}});
   }
 }
 export default PocService;
