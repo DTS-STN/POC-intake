94d61fa909c7b2be09f947b3e96ee36a1e96e4dc
shewood <shewood01@gmail.com>
2020-04-27 12:27:27 -0400

    added some tests for the form page

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-<button class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Submit</button>
+<button class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" data-cy="submit-button">Submit</button>
 </template>
 
 <script>
