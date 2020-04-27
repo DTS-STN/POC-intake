99c6bb2e527fe6478ee02355bc0cb177be4c41f7
Mr. Pond <32133001+P0NDER0SA@users.noreply.github.com>
2020-04-27 17:03:08 -0400

    Update PocService.js

--- requests/PocService.js
+++ requests/PocService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api=`${process.env.VUE_APP_API_URL}` ? `${process.env.VUE_APP_API_URL}` : "http://localhost:3000/api/" ;
+const api=`${process.env.VUE_APP_API_URL}`;
 console.log(`using API: ${api}`);
 class PocService{
   static insertPoc(fname, lname, email, message){
