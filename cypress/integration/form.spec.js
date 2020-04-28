22b3e26a1b27360ed67d80c6761a9953b23f2288
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-04-28 17:14:25 -0400

    fix test

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -4,31 +4,31 @@ describe('open to the form page', function() {
     beforeEach(function() {
       cy.visit('/en')
     })
-  
+
     it('Loads the form page when the get started button is clicked', () => {
-      // Test the page changes when the start button is clicked. 
+      // Test the page changes when the start button is clicked.
       cy.get('[data-cy=start-button]').should('be.visible')
       cy.get('[data-cy=start-button]').click()
       cy.url().should('include', '/en/insertpoc')
-  
+
   })
   })
   describe('open to the form page', function() {
     beforeEach(function() {
-      cy.visit('/en/insertpoc')
+      cy.visit('/en/InsertPocPage')
     })
     it('Form elements are shown on the page', () => {
-        // Test the page changes when the start button is clicked. 
+        // Test the page changes when the start button is clicked.
         cy.get('[data-cy=form-title]').should('be.visible')
         cy.get('[data-cy=fn-title]').should('be.visible')
         cy.get('[data-cy=ln-title]').should('be.visible')
         cy.get('[data-cy=email-title]').should('be.visible')
         cy.get('[data-cy=message-title]').should('be.visible')
         cy.get('[data-cy=submit-button]').should('be.visible')
-    
+
     })
     it('error messages are shown on the page is no text is entered into the field', () => {
-        // first name 
+        // first name
         cy.get('[data-cy=fn-text-entry]').should('be.visible')
          //last name
          cy.get('[data-cy=ln-text-entry]').should('be.visible')
@@ -43,10 +43,9 @@ describe('open to the form page', function() {
         cy.get('[data-cy=ln-error] > span').should('be.visible')
         cy.get('[data-cy=email-error]').should('be.visible')
         cy.get('[data-cy=message-error] > span').should('be.visible')
-    
+
     })
-    
-    
+
+
 
 })
-  
