c8b10724be198ea6ef40fefd76a0dc80ebf1f0c2
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-04-29 16:56:26 -0400

    temporarily hardcding api an dfixing a small bug

--- services/APIService.js
+++ services/APIService.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
+;
+const api = process.env.NODE_ENV=='production' ? "https://poc-intake.dev.dts-stn.com/api/" : "http://localhost:3000/api/";
+console.log( proce.en.NODE_ENV );
+console.log(`using api: ${api}`);
 
 class APIService{
-  static insertPoc(url, fname, lname, email, message){
+  static insertPoc(fname, lname, email, message){
     return axios.post(`${api}insertpoc`, null, { params: {fname: fname, lname: lname, email: email, message: message} });
   }
 
-  static getAllPocs(url) { return axios.get(`${url}api/allpocs`); }
+  static getAllPocs() { return axios.get(`${api}allpocs`); }
 }
 export default APIService;
