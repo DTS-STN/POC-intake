d95254085cb24bf3ccb1b7edcd8bc32b943f0cd8
Mr. Pond <32133001+P0NDER0SA@users.noreply.github.com>
2020-04-30 15:00:36 -0400

    Update APIService.js
    gotta fix this var

--- services/APIService.js
+++ services/APIService.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
+import axios from 'axios';
 
-const api = process.env.VUE_APP_API_URL;
-console.log( process.env.NODE_ENV );
+const api = `${process.env.VUE_APP_API_URL}`;
 console.log(`using api: ${api}`);
+
+class APIService{
+  static insertPoc(fname, lname, email, message){
+    return axios.post(`${api}insertpoc`, null, { params: {fname: fname, lname: lname, email: email, message: message} });
+  }
+
+  static getAllPocs() { return axios.get(`${api}allpocs`); }
+}
+export default APIService;
+
 
 class APIService{
   static insertPoc(fname, lname, email, message){
