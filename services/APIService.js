e1ba839e111a7327767a42d517312ed77c71ebdd
Mr. Pond <32133001+P0NDER0SA@users.noreply.github.com>
2020-04-30 15:10:06 -0400

    Update APIService.js
    fixing!!!

--- services/APIService.js
+++ services/APIService.js
@@ -11,13 +11,3 @@ class APIService{
   static getAllPocs() { return axios.get(`${api}allpocs`); }
 }
 export default APIService;
-
-
-class APIService{
-  static insertPoc(fname, lname, email, message){
-    return axios.post(`${api}insertpoc`, null, { params: {fname: fname, lname: lname, email: email, message: message} });
-  }
-
-  static getAllPocs() { return axios.get(`${api}allpocs`); }
-}
-export default APIService;
