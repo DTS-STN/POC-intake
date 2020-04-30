b39fbcf70dfe81b74ed007ac65e991317b121019
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-04-30 15:47:58 -0400

    we need to configure nuxt to return the api var fro mthe server into the front end

--- services/APIService.js
+++ services/APIService.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const api = `${process.env.VUE_APP_API_URL}`;
+const api = `${process.env.baseUrl}`;
 console.log(`using api: ${api}`);
-console.log(`api var: ${process.env.VUE_APP_API_URL}`);
-console.log(`conn str var: ${process.env.VUE_APP_CONNECTION_STRING}`);
+
 
 class APIService{
   static insertPoc(fname, lname, email, message){
