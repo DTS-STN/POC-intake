f7ab04581597b5d78cc79a63a4918618b6c402ae
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-05-05 15:54:22 -0400

    adding a plugin to mimic a main.js file

---
+++ plugins/global.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import AppButton from "@/components/AppButton";
+import AppTitles from "@/components/AppTitles";
+
+Vue.component("AppButton", AppButton);
+Vue.component("AppTitles", AppTitles);
