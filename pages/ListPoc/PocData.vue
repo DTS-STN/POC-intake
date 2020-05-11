b47f080a30d8f93ca5ed2dd0d6169f8ec22f74ee
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2020-05-11 09:18:53 -0400

    saving updates to listpoc

--- pages/ListPoc/PocData.vue
+++ pages/ListPoc/PocData.vue
@@ -4,17 +4,17 @@
       <div class="datagrid">
         <div class="dataCell">
           <div class="celldiv">
-            {{ poc.fname }}
+            <span class="text-black">name :&nbsp; </span> {{ poc.fname }}
           </div>
           <div class="celldiv">
-            {{ poc.lname }}
+            <span class="text-black">lastname :&nbsp; </span> {{ poc.lname }}
           </div>
         </div>
         <div class="dataCell">
-          {{ poc.email }}
+          <span class="text-black">email :&nbsp; </span> {{ poc.email }}
         </div>
         <div class="dataCell description">
-          {{ poc.message }}
+          <span class="text-black">message :&nbsp; </span> {{ poc.message }}
         </div>
       </div>
     </div>
@@ -41,10 +41,10 @@ export default {
   @apply font-sans mt-2 flex-auto justify-center items-center text-blue-600 mx-auto w-11/12 min-w-0 h-auto min-h-0;
 }
 .datagrid {
-  @apply border-solid border border-blue-600 text-center w-full min-w-full h-auto min-h-0 p-1 mb-2;
+  @apply border-solid border border-blue-600 text-center w-full min-w-full h-auto min-h-0 p-1 mb-6 shadow-lg rounded;
 }
 .dataCell {
-  @apply flex flex-wrap  mt-2 mb-2;
+  @apply flex flex-wrap  mt-2 mb-2 pl-2;
 }
 .celldiv {
   @apply flex mr-1 w-5/12 min-w-0;
