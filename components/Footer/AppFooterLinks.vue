3e575d498fab0496fda0cd0385176784047cc5e5
Benoit Cloutier <cloutier.ben@yahoo.com>
2020-05-21 12:00:23 -0400

    Changed the footer styling for flex boxes. Easier to read with less code.

--- components/Footer/AppFooterLinks.vue
+++ components/Footer/AppFooterLinks.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="brand">
-      <div class="container-links">
-        <div class="row" >
-
-          <nav id="footer-nav" class="col-md-10 ftr-urlt-lnk" > 
-
-            <!-- <h2 class="wb-inv" style="background-color: gray; clip: rect(1px,1px,1px,1px); height: 1px; margin: 0; overflow: hidden; position: absolute; width: 1px;"> {{ $t('footer.site') }}</h2> -->
-
-            <ul id="fLinks">
-              <li><a href="https://www.canada.ca/en/social.html"> {{ $t('footer.social') }} </a></li>
-              <li><a href="https://www.canada.ca/en/mobile.html"> {{ $t('footer.mobile') }} </a></li>
-              <li><a href="https://www.canada.ca/en/government/about.html"> {{ $t('footer.about') }} </a></li>
-              <li><a href="https://www.canada.ca/en/transparency/terms.html"> {{ $t('footer.terms') }} </a></li>
-              <li><a href="https://www.canada.ca/en/transparency/privacy.html"> {{ $t('footer.privacy') }} </a></li>
-              <li><a href="https://home.dts-stn.com/start.html"> {{ $t('footer.dts') }} </a></li>
-            </ul>
-          </nav>
-          
-          <div class="col-xs-6 visible-sm visible-xs tofpg   hidden  md:visible " style="line-height: 90px;">
-            <a href="#nav-top-page"> {{ $t('footer.top') }} <span class="glyphicon glyphicon-chevron-up"></span></a>
+  <div id="brand">
+        <div id="container-links">
+          <div id="row" >
+   
+            <nav id="footer-nav"> 
+  
+              <!-- <h2 class="wb-inv" style="background-color: gray; clip: rect(1px,1px,1px,1px); height: 1px; margin: 0; overflow: hidden; position: absolute; width: 1px;"> {{ $t('footer.site') }}</h2> -->
+  
+              <ul id="fLinks">
+                <li><a href="https://www.canada.ca/en/social.html"> {{ $t('footer.social') }} </a></li>
+                <li><a href="https://www.canada.ca/en/mobile.html"> {{ $t('footer.mobile') }} </a></li>
+                <li><a href="https://www.canada.ca/en/government/about.html"> {{ $t('footer.about') }} </a></li>
+                <li><a href="https://www.canada.ca/en/transparency/terms.html"> {{ $t('footer.terms') }} </a></li>
+                <li><a href="https://www.canada.ca/en/transparency/privacy.html"> {{ $t('footer.privacy') }} </a></li>
+                <li><a href="https://home.dts-stn.com/start.html"> {{ $t('footer.dts') }} </a></li>
+              </ul>
+            </nav>
+            
+            <div id="footer-top-page">
+              <a href="#nav-top-page">{{ $t('footer.top') }}<span class="glyphicon glyphicon-chevron-up"></span></a>
+            </div>
+  
+            <div id="footer-img">
+              <img id="cnd-img" src="~/assets/img/canada_footer_logo.svg" :alt=" $t('footer.symbol') " :title=" $t('footer.symbol') " />
+            </div>
+  
           </div>
-
-          <div class="col-xs-6 col-md-2 text-right">
-            <img src="~/assets/img/canada_footer_logo.svg" :alt=" $t('footer.symbol') " :title=" $t('footer.symbol') " />
-          </div>
-
         </div>
       </div>
-    </div>
 </template>
 
 <script>
@@ -37,54 +37,48 @@ export default {
 </script>
 
 <style>
-
-.container-links { 
+#container-links { 
   padding-right: 15px;
   padding-left: 15px;
   margin-right: auto;
   margin-left: auto;
 } 
-.container-links::after { clear: both; }
 
-.row {
-  margin-right: -15px;
-  margin-left: -15px;
-}
-
-.row::after {
-  clear: both;
-}
-.col-md-10, .col-xs-6, .col-md-2 {
-  position: relative;
-  min-height: 1px;
-  padding-right: 15px;
-  padding-left: 15px; 
-}
-.col-xs-6 { float: left; width: 50%; }
-
-@media (min-width: 992px) {
-  .col-md-2  { float: left; width: 16.6666666667%; }
-  .col-md-10 { float: left; width: 83.3333333333%; }
-  
-}
-#wb-info .brand {
+#brand {
   background: #f8f8f8;
   line-height: 30px;
 }
-#wb-info .brand img {
+
+#row {
+  display: flex;
+  flex-wrap: wrap;
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+#footer-nav {
+  padding-top: 2em;
+  padding-bottom: 2em;
+  position: relative;
+  width: 100%;
+}
+
+#footer-top-page{
+  width: 50%;
+}
+
+#footer-img{
+  width: 50%;
+}
+
+#cnd-img {
   height: 40px;
   margin-bottom: 10px;
   margin-top: 20px;
   width: auto;
+  float: right;
 }
-#wb-info #footer-nav {
-  padding-top: 2em;
-  padding-bottom: 2em;
-  position: relative;
-}
-#wb-info .ftr-urlt-lnk li::before {
-	content: "•";
-}
+
 #fLinks {
   display: grid;
   grid-template-columns: auto;
@@ -93,6 +87,13 @@ export default {
   margin-bottom: 11.5px;
   font-size: 14px;
   color: #333;
+  padding: 0;
+  list-style-type: none;
+}
+
+#fLinks li::before {
+	content: "•";
+  margin-right: .7em;
 }
 
 .glyphicon {
@@ -106,60 +107,32 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-.glyphicon-chevron-up:before {
+.glyphicon-chevron-up::before {
   content: "^"; 
 }
 
-@media   (min-width: 480px) {
-  #wb-info .ftr-urlt-lnk #fLinks {
-
-    grid-template-columns: auto auto;
-    
-  }
-  #wb-info .ftr-urlt-lnk li {
-    display: block;
-    float: none;
-    margin-bottom: .2em;
-  }
-  #wb-info .tofpg {
-    line-height: 60px; }
-}
-@media (min-width: 480px) and (max-width: 1025px) {
-  #wb-info .ftr-urlt-lnk #fLinks {
-    -webkit-column-count: 2 !important;
-    column-count: 2 !important;
-  }
-  #wb-info .ftr-urlt-lnk li {
-    display: block;
-    float: none;
-    margin-bottom: .2em; 
-  }  
-} 
-
-@media (max-width: 768px) {
-  .visible-xs { display: block !important; }
-}
-@media  (max-width: 991px) {
-  .visible-xs { display: block !important; }
-  #wb-info .brand img { float: right; }
-} 
-
-@media screen and (min-width: 1030px) {
+@media (min-width: 350px) {
   #fLinks{
-   display: flex;
-   align-content: flex-start;
-   justify-content: space-evenly;   
+    grid-template-columns: auto auto;
   }
-  #wb-info #footer-nav {
-    padding: 2em 0;
-  } 
 }
-@media (min-width: 1030px) and (max-width: 1200px) { 
-  li::before{
-    content: '' !important;
-  } }
-@media (min-width:  768px) { .container-links { width:  750px; } }
-@media (min-width:  992px) { .container-links { width:  970px; } }
-@media (min-width: 1200px) { .container-links { width: 1170px; } }
 
+@media (min-width: 1200px) {
+  #footer-top-page{ display: none; }
+  #footer-img { width: 16.6666666667%; }
+  #footer-nav { width: 83.3333333333%; }
+  #fLinks{ 
+    display: flex;
+    align-content: flex-start;
+    justify-content: space-evenly;   
+  }
+  #fLinks li:first-child:before{
+    content: " ";
+  }
+  
+}
+
+@media (min-width:  768px) { #container-links { width:  750px; } }
+@media (min-width:  992px) { #container-links { width:  970px; } }
+@media (min-width: 1200px) { #container-links { width: 1170px; } }
 </style>
