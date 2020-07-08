3a9416d233c0339d0f392dcebbc8ab50de415edd
Ben Larabie <benlarabie@gmail.com>
2020-07-08 15:04:57 -0400

    Switching to dts-stn.com domain

--- helmfile/context-dev.sh
+++ helmfile/context-dev.sh
@@ -8,7 +8,7 @@ if [ -z "$DOCKER_TAG" ]
 then
     export DOCKER_TAG=latest
 fi
-export BASE_DOMAIN=dts-stn.dev
+export BASE_DOMAIN=dts-stn.com
 export GITHUB_USER=$(az keyvault secret show --vault-name DTSSecrets --name dts-github-user --query value -otsv)
 export GITHUB_TOKEN=$(az keyvault secret show --vault-name DTSSecrets --name dts-github-token --query value -otsv)
 export VUE_APP_POC_INTAKE_DB_PASSWORD=$(az keyvault secret show --vault-name DTSSecrets --name dts-poc-intake-vue-db-pass-dev --query value -otsv)
